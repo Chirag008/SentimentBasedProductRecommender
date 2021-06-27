118b3212344d7f0fce459931fad3df61edadf1a6
Chirag008 <chirag.bansal17@gmail.com>
2021-06-27 09:53:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,6 @@ def recommend_top_5_products():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    #app.run(host='0.0.0.0', port=5000)
+    print('*** App Started ***')
+    app.run(debug=True)
